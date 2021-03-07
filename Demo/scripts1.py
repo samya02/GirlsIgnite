718988ab7d719c1417fd0ab5ccaf22883f2a7429
Kashika Akhouri <kashika0112@gmail.com>
2021-03-07 14:53:02 +0530

    Files addition

--- Demo/scripts1.py
+++ Demo/scripts1.py
@@ -10,5 +10,11 @@ def home():
 def about():
     return render_template("about.html")
 
+
+@app.route('/blog/')
+def blog():
+    return render_template("blog.html")
+
+
 if __name__=="__main__":
     app.run(debug=True)
